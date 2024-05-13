f12e6b7ad6c47f7e132129a8ddaec42eb5f3dd3a
Houda Rais <houdarais@Houdas-Air.fios-router.home>
2024-05-13 15:10:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+import { useEffect } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "timer":
+      return { seconds: state.seconds + 1, running: true };
+    case "start":
+      return { seconds: state.seconds + 1, running: true };
+    case "stop":
+      return { seconds: state.seconds + 1, running: false };
+
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { seconds: 0, running: false });
+  useEffect(() => {
+    let interval;
+    if (state.running) {
+      interval = setInterval(() => {
+        dispatch({ type: "timer" });
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [state.running]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="btnContainer">
+        <button
+          onClick={() => {
+            dispatch({ type: "start" });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Start Timer
+        </button>
+
+        <button
+          onClick={() => {
+            dispatch({ type: "stop" });
+          }}
+        >
+          Stop Timer
+        </button>
+      </div>
+      <h2> Timer: {state.seconds}s </h2>
     </div>
   );
 }
